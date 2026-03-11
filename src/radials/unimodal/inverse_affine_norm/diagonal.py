89ce00e0cf60ce14c92c1a516d8da3de9b07a9b2
Willem Diepeveen <willem_diep@hotmail.com>
2026-03-10 17:03:12 -0700

    minor changes

--- src/radials/unimodal/inverse_affine_norm/diagonal.py
+++ src/radials/unimodal/inverse_affine_norm/diagonal.py
@@ -4,14 +4,15 @@ import torch.nn as nn
 from src.radials.unimodal.inverse_affine_norm import InverseAffineNormRadial
 
 class InverseDiagonalNormRadial(InverseAffineNormRadial):
-    def __init__(self, d, sigma=1e-6):
+    def __init__(self, d, diagonal=None sigma=1e-6):
         """
         
         :param centered: bool, whether the linear layer should have a bias term
         """
         super().__init__(d, sigma=sigma)
-
-        self.diagonal = nn.Parameter(torch.randn(self.d).exp())
+        if diagonal is None:
+            self.diagonal = nn.Parameter(torch.randn(self.d).exp())
+            
 
     def affine(self, theta):
         return self.diagonal * theta
