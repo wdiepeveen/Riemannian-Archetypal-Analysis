00a8b7b91a381570ae9d48d59f417dfed9879dcd
wdiepeveen <willem_diep@hotmail.com>
2026-03-18 16:32:35 -0700

    pipeline works, but for mnist the distribution is made too normal probably

--- src/training/trimmed_ellipsoid_starflow.py
+++ src/training/trimmed_ellipsoid_starflow.py
@@ -36,33 +36,43 @@ class TrimmedEllipsoidStarFlowTraining(torch.nn.Module):
     def star_center(self):
         return self._star_center.reshape(*self.shape)
 
-    def fit(self, data):
+    def fit(self, data, star_center=None):
         N = data.shape[0]
         assert list(data.shape[1:]) == self.shape, f"Data shape {list(data.shape[1:])} does not match expected shape {self.shape}"
 
         # Step 1: K-means clustering
         kmeans = KMeans(n_clusters=self.K, random_state=0).fit(data.reshape(N, -1).numpy())
-        self.cluster_labels = torch.from_numpy(kmeans.labels_).to(data.dtype)
-        self._cluster_centers = torch.from_numpy(kmeans.cluster_centers_).to(data.dtype)
+        cluster_labels = torch.from_numpy(kmeans.labels_).to(data.dtype)
+        cluster_centers_ = torch.from_numpy(kmeans.cluster_centers_).to(data.dtype)
 
         # Step 2: Compute cluster covariances
-        self._cluster_covariances = torch.zeros((self.K, self.d, self.d), dtype=data.dtype)
-        self.cluster_sizes = []
+        cluster_centers = []
+        cluster_covariances = []
+        cluster_sizes = []
         for k in range(self.K):
-            cluster_data = data.reshape(N, -1)[self.cluster_labels == k]
-            cluster_center = self._cluster_centers[k]
+            cluster_data = data.reshape(N, -1)[cluster_labels == k]
+            cluster_center = cluster_centers_[k]
             cluster_size = cluster_data.shape[0]
-            self._cluster_covariances[k] = torch.einsum('ni,nj->ij', cluster_data - cluster_center, cluster_data - cluster_center) / (cluster_size - 1) + self.cov_reg * torch.eye(self.d, dtype=data.dtype)
-            self.cluster_sizes.append(cluster_size)
+            if cluster_size > 1:
+                cluster_centers.append(cluster_center)
+                cluster_covariances.append(torch.einsum('ni,nj->ij', cluster_data - cluster_center, cluster_data - cluster_center) / (cluster_size - 1) + self.cov_reg * torch.eye(self.d, dtype=data.dtype))
+                cluster_sizes.append(cluster_size)
+            else: 
+                self.K -= 1
 
+        self._cluster_covariances = torch.stack(cluster_covariances)
+        self._cluster_centers = torch.stack(cluster_centers)
+        self.cluster_sizes = torch.tensor(cluster_sizes, dtype=data.dtype)
         self._inv_cluster_covariances = torch.cat([torch.linalg.inv(cov)[None] for cov in self._cluster_covariances], dim=0)
-        self.weights = torch.tensor(self.cluster_sizes, dtype=data.dtype) / N
+        self.weights = self.cluster_sizes / N
 
         # Step 3: Compute star center
-        A = (self.weights[:, None, None] * self._inv_cluster_covariances).sum(dim=0)
-        b = (self.weights[:, None] * torch.einsum('nij,nj->ni', self._inv_cluster_covariances, self._cluster_centers)).sum(dim=0)
-        
-        self._star_center = torch.linalg.solve(A, b)
+        if star_center is not None:
+            self._star_center = star_center.reshape(-1)
+        else:
+            A = (self.weights[:, None, None] * self._inv_cluster_covariances).sum(dim=0)
+            b = (self.weights[:, None] * torch.einsum('nij,nj->ni', self._inv_cluster_covariances, self._cluster_centers)).sum(dim=0)
+            self._star_center = torch.linalg.solve(A, b)
 
         # Step 4: Construct starflow distribution
         self.star = MultiModalEllipsoidStarDistribution(covs=self._cluster_covariances, 
