20b5da4ad5485ccce4a77aac36b99c2e91245dd5
wdiepeveen <willem_diep@hotmail.com>
2026-04-09 14:56:16 -0700

    more tests on MNIST

--- src/training/stars/__init__.py
+++ src/training/stars/__init__.py
@@ -4,14 +4,15 @@ from src.dimension_reduction.convex_simplex_structured_matrix_factorization impo
 from src.distributions.stars.ellipsoid.multimodal import MultiModalEllipsoidStarDistribution
 
 class StarTraining:
-    def __init__(self, shape, n_clusters, n_max=-1, c=4/3, p=0.95, trimmed=False, cov_reg=1e-6):
+    def __init__(self, shape, n_clusters, N_max=-1, c=4/3, p=0.95, trimmed=False, cov_reg=1e-6):
         super(StarTraining, self).__init__()
 
         self.d = torch.prod(torch.tensor(shape)).item()
         self.shape = shape
 
-        self.K = n_clusters
-        self.n_max = n_max
+        self.r = n_clusters
+        self.N_max = N_max
+        self.k = n_clusters
         
         self.c = c
         self.p = p
@@ -34,15 +35,17 @@ class StarTraining:
     def end_members(self):
         return self._end_members.reshape(-1, *self.shape)
 
-    def fit(self, data, labels=None):
+    def fit(self, data, labels=None, min_cluster_size=None):
         N = data.shape[0]
+        if min_cluster_size is None:
+            min_cluster_size = self.d + 1  # Minimum size to compute covariance
         assert list(data.shape[1:]) == self.shape, f"Data shape {list(data.shape[1:])} does not match expected shape {self.shape}"
 
         # Step 1: CSSMF clustering
         if labels is None:
             X = data.reshape(N, -1).T  # use the generated data as input
             solver = ConvexSimplexStructuredMatrixFactorizationSolver(d=X.shape[0], N=X.shape[1])
-            solver.fit(X, r=self.K, n_max=self.n_max, max_iter=500)
+            solver.fit(X, r=self.r, N_max=self.N_max, max_iter=500)
             self._end_members = solver.V.T
             labels = solver.labels.cpu()
 
@@ -50,10 +53,10 @@ class StarTraining:
         cluster_centers = []
         cluster_covariances = []
         cluster_sizes = []
-        for k in range(self.K):
+        for k in range(self.r):
             cluster_data = data.reshape(N, -1)[labels == k]
             cluster_size = cluster_data.shape[0]
-            if cluster_size > self.d:  # Ensure we have enough data points to compute covariance
+            if cluster_size >= min_cluster_size:  # Ensure we have enough data points to compute covariance
                 cluster_sizes.append(cluster_size)
 
                 cluster_mu = cluster_data.mean(dim=0)
@@ -63,7 +66,7 @@ class StarTraining:
                 cluster_covariances.append(cluster_cov)
                 
             else: 
-                self.K -= 1
+                self.k -= 1
 
         self._cluster_covariances = torch.stack(cluster_covariances)
         self._cluster_centers = torch.stack(cluster_centers)
