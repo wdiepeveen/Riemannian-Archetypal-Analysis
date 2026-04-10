7a0cb1294acc3c799ea42f6300d2eecce6596194
wdiepeveen <willem_diep@hotmail.com>
2026-04-10 16:23:18 -0700

    minor changes

--- src/manifolds/hyperbolic/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.manifolds import Manifold
-
-class Hyperbolic(Manifold):
-    def __init__(self, d):
-        super().__init__(d)
--- src/manifolds/hyperbolic/vector/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.manifolds.hyperbolic import Hyperbolic
-
-class VectorHyperbolic(Hyperbolic):
-    def __init__(self, d):
-        super().__init__(d)
