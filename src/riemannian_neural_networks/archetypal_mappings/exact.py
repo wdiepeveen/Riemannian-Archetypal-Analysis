b933cc1422c08ac3e10ef9c8369102d6bd1228ee
wdiepeveen <willem_diep@hotmail.com>
2026-04-23 11:50:15 -0700

    success

--- src/riemannian_neural_networks/archetypal_mappings/exact.py
+++ src/riemannian_neural_networks/archetypal_mappings/exact.py
@@ -8,20 +8,35 @@ class ExactRiemannianArchetypalMapping(RiemannianArchetypalMapping):
         super().__init__(euclidean_pullback_manifold, archetypes, max_iter=max_iter, tol=tol, accelerated=accelerated, line_search=line_search, ls_beta=ls_beta, ls_c=ls_c, ls_max_iter=ls_max_iter)
         self.phi = self.manifold.phi
         self.phi_m = self.phi(self.m) # (r, d)
-        if init_euclidean_pullback_manifold is not None:
-            self.init_manifold = init_euclidean_pullback_manifold
-            self.relaxed_ram = RelaxedRiemannianArchetypalMapping(self.init_manifold, self.m, max_iter=self.max_iter, tol=self.tol, accelerated=self.accelerated) 
-        else:
-            self.relaxed_ram = RelaxedRiemannianArchetypalMapping(self.manifold, self.m, max_iter=self.max_iter, tol=self.tol, accelerated=self.accelerated)
+        self.init_manifold = init_euclidean_pullback_manifold
 
-        self.initial_step_size = self.relaxed_ram.initial_step_size
+        # step size
+        G = self.phi_m.reshape(self.r, self.d) @ self.phi_m.reshape(self.r, self.d).T
+        evals = torch.linalg.eigvalsh(G)
+        L = 2.0 * torch.clamp(evals[-1], min=1e-12)
+        self.initial_step_size = 1.0 / L
     
     def archetype_weights_init(self, x):
         """
         :param x: N x [input_dim] tensor
         :return: N x r tensor of archetypal coefficients
         """
-        return self.relaxed_ram.archetype_weights_init(x)
+        if self.init_manifold is not None:
+            relaxed_ram = RelaxedRiemannianArchetypalMapping(self.init_manifold, self.m, max_iter=self.max_iter, tol=self.tol, accelerated=self.accelerated) 
+        else:
+            relaxed_ram = RelaxedRiemannianArchetypalMapping(self.manifold, self.m, max_iter=self.max_iter, tol=self.tol, accelerated=self.accelerated)
+
+        return relaxed_ram.archetype_weights_init(x)
+    
+    # def archetype_weights_init(self, x):
+    #     """
+    #     :param x: N x [input_dim] tensor
+    #     :return: N x r tensor of archetypal coefficients
+    #     """
+    #     N = x.shape[0]
+    #     phi_x = self.phi(x)  # (N, d)
+    #     pairwise_distances = torch.cdist(phi_x.reshape(N, self.d), self.phi_m.reshape(self.r, self.d))  # (N, r)
+    #     return torch.softmax(-pairwise_distances, dim=-1)
     
     def objective(self, w, x):
         """
